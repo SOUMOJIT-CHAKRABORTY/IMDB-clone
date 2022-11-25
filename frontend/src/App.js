6cf1455c465872abc61d67f142b4ea6b1388f51d
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2022-11-26 04:01:25 +0530

    Few components created and some need to be created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
-          <Route index element={<h1>Hello World</h1>}></Route>
+          <Route index element={<Home />}></Route>
           <Route path="movie/:id" element={<h1>Movie Details Page</h1>}></Route>
           <Route
             path="movies/:type"
