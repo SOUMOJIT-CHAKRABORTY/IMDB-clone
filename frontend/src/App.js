f94dfe5c36078429b693d9e667062586ac85afd2
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2022-11-25 23:37:57 +0530

    basic structure is created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <Routes>
+          <Route index element={<h1>Hello World</h1>}></Route>
+          <Route path="movie/:id" element={<h1>Movie Details Page</h1>}></Route>
+          <Route
+            path="movies/:type"
+            element={<h1>Movies List Page</h1>}
+          ></Route>
+          <Route path="/*" element={<h1>Error Page</h1>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
